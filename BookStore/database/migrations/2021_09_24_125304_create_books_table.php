ed26ce05b54dcff1952572f736607e45378e757f
sajidbaloch1 <kinglovers109@gmail.com>
2021-09-24 18:12:11 +0500

    -laravel Isu1 as complete and i will complete migration tables

---
+++ BookStore/database/migrations/2021_09_24_125304_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->integer('price');
+            $table->string('discreption');
+            $table->integer('category_id');
+            $table->integer('author_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
