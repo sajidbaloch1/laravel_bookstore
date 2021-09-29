ffd5aca4362cc7f13ac7e97a7d93544ab18af4f9
sajidbaloch1 <kinglovers109@gmail.com>
2021-09-29 15:54:42 +0500

    login and signup page has complete

--- BookStore/app/Http/Controllers/SignupController.php
+++ BookStore/app/Http/Controllers/SignupController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SignupController extends Controller
 {
@@ -10,4 +12,12 @@ class SignupController extends Controller
         return view('login.signup');
     }
     //
+    public function signupConfirmed(Request $req){
+        $user = new User();
+        $user->name = $req->name;
+        $user->email = $req->email;
+        $user->password = Hash::make($req->password);
+        $user->save();
+        return redirect('/');
+    }
 }
--- BookStore/resources/views/login/login.blade.php
+++ BookStore/resources/views/login/login.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.layouts')
-@section('title','Login')
+@section('title', 'Login')
 @section('content')
-<div class="container">
-    <h1 class="text-center" style="background: rgb(2, 0, 36);
-    background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(20, 20, 20, 0.9640231092436975) 35%, rgba(0, 212, 255, 1) 100%); color: white;">LOGIN</h1>
+    <div class="container">
+        <h1 class="text-center"
+            style="background: rgb(2, 0, 36);
+        background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(20, 20, 20, 0.9640231092436975) 35%, rgba(0, 212, 255, 1) 100%); color: white;">
+            LOGIN</h1>
         <div class="row">
             <div class="col-lg-12">
-                <form action="" method="POST">
+                <form action="{{ route('login-confirmed') }}" method="POST">
                     @csrf
-                <div class="form-floating mb-3">
-      <input type="email" name="email" class="form-control my-5" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" name="password" class="form-control my-5" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-      <input type="submit" value="Submit" class="btn btn-success float-end">
-    </div>
+                    <div class="form-floating mb-3">
+                        <input type="email" name="email" class="form-control my-5" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control my-5" id="floatingPassword"
+                            placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                        <input type="submit" value="Submit" class="btn btn-success float-end">
+                    </div>
                 </form>
             </div>
         </div>
     </div>
-    
+
 @endsection
